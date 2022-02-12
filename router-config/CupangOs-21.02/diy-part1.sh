aa7375915b655702c7eec244f2bfc17cd8de0e3c
robbyaprianto <robbyaprianto@hotmail.com>
2022-02-13 00:31:25 +0700

    bismillah lancar

---
+++ router-config/CupangOs-21.02/diy-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: 21.02
+#========================================================================================================================
+
+# add feed for theme thano
+# sed -i '$a src-git system https://github.com/riyuejz/system.git;main' feeds.conf.default
+
+# Add a cupangOs-packages feed source
+#sed -i '$a src-git small https://github.com/kenzok78/small-package' feeds.conf.default
+sed -i '$a src-git cupangOs_packages https://github.com/thecupangin/cupangOs-packages' feeds.conf.default
+
+# other
+#rm -rf package/lean/{samba4,luci-app-samba4,*docker*}
+#rm -rf package/{samba4,luci-app-samba4,*docker*}

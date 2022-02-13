44c873cc91fe20bcd9fd567a82f2b2cc171bcd6c
robbyaprianto <robbyaprianto@hotmail.com>
2022-02-13 13:33:59 +0700

    update 13feb2022 1:33PM

--- router-config/CupangOs-21.02/diy-part1.sh
+++ router-config/CupangOs-21.02/diy-part1.sh
@@ -9,10 +9,14 @@
 # add feed for theme thano
 # sed -i '$a src-git system https://github.com/riyuejz/system.git;main' feeds.conf.default
 
+# Clone Lean's feeds
+sed -i '$a src-git lean_packages https://github.com/coolsnowwolf/packages' feeds.conf.default
+sed -i '$a src-git lean_luci https://github.com/coolsnowwolf/luci customfeeds/luci' feeds.conf.default
+
 # Add a cupangOs-packages feed source
 #sed -i '$a src-git small https://github.com/kenzok78/small-package' feeds.conf.default
 sed -i '$a src-git cupangOs_packages https://github.com/thecupangin/cupangOs-packages' feeds.conf.default
 
 # other
 #rm -rf package/lean/{samba4,luci-app-samba4,*docker*}
-#rm -rf package/{samba4,luci-app-samba4,*docker*}
+#rm -rf package/{samba4,luci-app-samba4,*docker*,luci-app-control*,*timecontrol*}
